183b4f36e663fb111af787b9053f6a565599a664
Aditya Deshmukh <adityad1405@gmail.com>
2025-09-28 21:12:05 +0530

    fix: make email field in create_table migration case-insensitive

--- cmd/migrate/20250928132609_create_users_table.sql
+++ cmd/migrate/20250928132609_create_users_table.sql
@@ -1,11 +1,15 @@
 -- +goose Up
+-- Enable citext extension if not already enabled
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email CITEXT NOT NULL UNIQUE, -- case-insensitive email
     password_hash VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
 DROP TABLE users;
+
